6b5f5022a2ea6e041f554b5dc74219f69d31abb0
wanghaha <724147300@qq.com>
2019-09-27 23:43:14 +0800

    上次配置参数保留到 dat.config 文件，启动时读取配置文件，进行初始化配置

--- py/parameter_config_tool/main_tk.py
+++ py/parameter_config_tool/main_tk.py
@@ -22,6 +22,8 @@ class application(Frame):
         self.master.title(title)
         self.master.iconbitmap(relative_path_to_abs("fruit.ico"))
         self.pack()
+        
+        self.init_parameter()
         self.create_widgets()
         
     def run(self):
@@ -57,15 +59,11 @@ class application(Frame):
         ttk.Entry(fram,show=None, font=('Arial', 10),textvariable = self.key_second).pack(side = "left",padx=8, pady=4)
    
         ttk.Button(fram, text='打开解密文件', command=self.dat_decrypts).pack(side = "right",padx=8, pady=4)
-        
+
     def create_top_frame_first(self,p):
         top_frame = ttk.LabelFrame(p,text = "operation")       
-        top_frame.pack(expand = False,fill="x",side = "top",padx=8, pady=4)
+        top_frame.pack(expand = False,fill="x",side = "top",padx=8, pady=4)                                                
         
-        self.dat_op = StringVar()
-        self.dat_cmd = StringVar()
-        self.dat_tag = IntVar()
-              
         ttk.Label(top_frame, text = "op",width=3).grid(row=0, column=0,padx=8, pady=4)       
         temp = ttk.Combobox(top_frame,show=None,width=8, font=('Arial', 10),values = ("set","dump"),state='readonly',textvariable = self.dat_op)
         temp.grid(row=0, column=1)
@@ -91,8 +89,6 @@ class application(Frame):
     
         ttk.Label(parent, text = name,width=15).grid(row = index, column=0,padx=20)
         
-        self.datarr[index] = IntVar()
-        
         ttk.Entry(parent,show=None, font=('Arial', 10),foreground='red',textvariable = self.datarr[index]).grid(row = index, column=1,padx=8)
         
         ttk.Label(parent, text = unit,width=10).grid(row = index, column=2,padx=8)
@@ -102,13 +98,7 @@ class application(Frame):
     def create_mid_frame_first(self,p):
         mid_frame = ttk.LabelFrame(p,text = "information") 
         mid_frame.pack(expand = True,fill="both",side = "top",padx=8, pady=4)
-        
-        self.radvar = IntVar()
-        self.radvar.set(100)
-        
-        self.datlen = 14
-        self.datarr = {}
-        
+
         #----------------------------
         self.create_data_widget(mid_frame,0,"左仓余量","(0,10000)")
         #----------------------------
@@ -155,6 +145,37 @@ class application(Frame):
         self.key_first = StringVar()
         ttk.Label(bot_frame, text = "key").pack(side = "left",padx=8)
         ttk.Entry(bot_frame,show=None, font=('Arial', 10),textvariable = self.key_first).pack(side = "left",padx=10)
+    
+    def init_parameter(self):
+        
+        self.dat_dict = {}        
+        
+        try:
+            with open(relative_path_to_abs("dat.config"),"rb") as f:
+                self.dat_dict = json.load(f);
+                print(self.dat_dict)
+        except: #FileNotFoundError:
+            self.dat_dict = {"op": "set", "cmd": "all", "tag": 0, "par": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+            print("init ")
+            print(self.dat_dict)
+        
+        self.dat_op = StringVar()        
+        self.dat_cmd = StringVar()
+        self.dat_tag = IntVar()
+        
+        self.dat_op.set(self.dat_dict["op"])
+        self.dat_cmd.set(self.dat_dict["cmd"])
+        self.dat_tag.set(self.dat_dict["tag"])
+        
+        self.radvar = IntVar()
+        self.radvar.set(100)
+        
+        self.datlen = 14
+        self.datarr = {}
+        
+        for i in range(self.datlen):
+            self.datarr[i] = IntVar()
+            self.datarr[i].set(self.dat_dict["par"][i])
         
     def dat_decrypts(self):
 
@@ -181,6 +202,9 @@ class application(Frame):
         
         json_dict = {"op":self.dat_op.get(),"cmd":self.dat_cmd.get(),"tag":self.dat_tag.get()}
         
+        self.dat_dict["op"] = self.dat_op.get()
+        self.dat_dict["cmd"] = self.dat_cmd.get()
+        self.dat_dict["tag"] = self.dat_tag.get()
         
         if json_dict["op"] == "set":
             if json_dict["cmd"] == "all":
@@ -188,18 +212,20 @@ class application(Frame):
                 
                 for i in self.datarr:      
                     datarr[i] = self.datarr[i].get()  
-                    print(i)
+                    self.dat_dict["par"][i] = datarr[i]
                     
                 json_dict["par"] = datarr
             elif json_dict["cmd"] == "single":
                 
                 try:
-                    index = self.radvar.get()
-                    dat = self.datarr[index].get()                            
+                    index = self.radvar.get()                
+                    dat = self.datarr[index].get()  
+                    json_dict["index"] = index
                     json_dict["par"] = dat
+                    self.dat_dict["par"][index] = dat
                 except KeyError:
                     messagebox.showwarning("错误","请选择一个要配置的参数")
-                    
+                    return ""                      
         elif json_dict["op"] == "dump":  
             if json_dict["cmd"] == "all":
                 pass
@@ -208,8 +234,11 @@ class application(Frame):
             elif json_dict["cmd"] == "tag":
                 pass      
         
+        with open(relative_path_to_abs("dat.config"),"w") as f:
+            json.dump(self.dat_dict,f,indent=4)
+        
         json_file = open(relative_path_to_abs('dat.json'),'w')
-        json.dump(json_dict,json_file,indent=4)
+        json.dump(json_dict,json_file,indent=4) 
         json_file.close()
         
         json_str = json.dumps(json_dict)
@@ -220,6 +249,8 @@ class application(Frame):
     def dat_generate(self):
    
         json_str = self.dat_configuration()
+        if json_str == "":
+            return 
         
         if self.key_first.get() == "":
             key = "1234567891234567"
@@ -231,9 +262,11 @@ class application(Frame):
 
         encrypt_str = aes.encrypts(json_str)
         
-        #file = filedialog.asksaveasfilename(title='保存文件',filetypes=[("加密文件", "*.aes")],initialdir=relative_path_to_abs(""))
-        
-        with open(relative_path_to_abs("dat.json.aes"),"wb") as f:
+        file = filedialog.asksaveasfilename(title='保存文件',initialfile   = "dat.json.aes",filetypes=[("加密文件", ".aes")],initialdir=relative_path_to_abs(""))
+        if file == '':
+            return
+            
+        with open(file,"wb") as f:
             dat = f.write(encrypt_str)                
         
 
